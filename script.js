b4e14a9d760aecffc7488d565c9ebed7b3cfd94d
Jason Toups <jason.p.toups@gmail.com>
2018-03-30 10:31:12 -0400

    refactor code to separate classes a bit more

--- script.js
+++ script.js
@@ -26,9 +26,7 @@ class FlashCard {
       } else {
         this.result.innerText = 'Close, but not quite'
       }
-    } else {
-      console.log('display and front of card dont match')
-    }
+    } else {}
   }
   showHint () {
     // make the hint visible
@@ -41,94 +39,28 @@ class FlashCard {
     this.display.classList.remove('handwritten')
     this.display.style.fontSize = '100px'
   }
+  clear () {
+    // clear the hint, input, and result areas
+    this.input.value = ''
+    this.result.style.display = 'none'
+    this.hintText.innerHTML = ''
+  }
 }
 
 class Game {
   constructor () {
-    this.cards = [
-      new FlashCard('あ', 'a', 'an Apple with a stem'),
-      new FlashCard('い', 'i', 'two I\'s side by side'),
-      new FlashCard('う', 'u', 'man kicked in the stomach and flying sideways while saying \'Uuf\''),
-      new FlashCard('え', 'e', 'Energetic ninja running away'),
-      new FlashCard('お', 'o', 'ufO with an antenna'),
-      new FlashCard('か', 'ka', 'KAr driving off a cliff'),
-      new FlashCard('き', 'ki', 'old fashioned KEY'),
-      new FlashCard('く', 'ku', 'beak of a KUku bird'),
-      new FlashCard('け', 'ke', 'shape of a KEg'),
-      new FlashCard('こ', 'ko', 'KOi fish swimming around each other'),
-      new FlashCard('さ', 'sa', 'SAd face looking back at you'),
-      new FlashCard('し', 'shi', 'SHIp\'s anchor'),
-      new FlashCard('す', 'su', 'pregnant woman SOOn expecting'),
-      new FlashCard('せ', 'se', 'two SAIling ships passing each other'),
-      new FlashCard('そ', 'so', 'SEWing stitch'),
-      new FlashCard('た', 'ta', 'looks literally like TA'),
-      new FlashCard('ち', 'chi', 'CHEERleader mad at sa for being similar'),
-      new FlashCard('つ', 'tsu', 'tsunami wave'),
-      new FlashCard('て', 'te', 'dog\'s TAIl'),
-      new FlashCard('と', 'to', 'TOe with a hangnail'),
-      new FlashCard('な', 'na', 'someone frying tuNA'),
-      new FlashCard('に', 'ni', 'looks like a KNEE'),
-      new FlashCard('ぬ', 'nu', 'NOOdles with two chopsticks'),
-      new FlashCard('ね', 'ne', 'sNAIl hiding behind a NAIl'),
-      new FlashCard('の', 'no', 'NO entry sign'),
-      new FlashCard('は', 'ha/wa', 'capital H with a lowercase A under it = HA'),
-      new FlashCard('ひ', 'hi', 'smiling mouth saying hi hi hi'),
-      new FlashCard('ふ', 'fu', 'nose smelling some FUmes'),
-      new FlashCard('へ', 'he', 'pointing to HEaven'),
-      new FlashCard('ほ', 'ho', 'HO spelled on its side and a line underneath'),
-      new FlashCard('ま', 'ma', 'MAn wearing a MAsk'),
-      new FlashCard('み', 'mi', 'like a musical note MI'),
-      new FlashCard('む', 'mu', 'a pierced cow\'s nose that says MUUUU'),
-      new FlashCard('め', 'me', 'same as nu but dropped some noodles and made a MEss'),
-      new FlashCard('も', 'mo', 'koMOdo dragon with a long tail'),
-      new FlashCard('や', 'ya', 'birds on a wiYA outside your YArd'),
-      new FlashCard('ゆ', 'yu', 'Jesus fish but spelled in Greek YUsus'),
-      new FlashCard('よ', 'yo', 'YO-YO dangling from a finger'),
-      new FlashCard('ら', 'ra', 'RAbbit bouncing away'),
-      new FlashCard('り', 'ri', 'looks like a RIver. Longer right stroke. '),
-      new FlashCard('る', 'ru', 'RUdder of a ship turning leaving a wake'),
-      new FlashCard('れ', 're', 'snake on the cross to REvive patients'),
-      new FlashCard('ろ', 'ro', 'rear with a ship with less wake because it\'s being ROwed'),
-      new FlashCard('わ', 'wa', ''),
-      new FlashCard('を', 'wo', 'WOman walking across a street'),
-      new FlashCard('ん', 'n', 'looks like a letter N')
-      // second javascript file that has a traditional javascript object. Array of objects
-      // Loop through the array of objects and create an instance of each object as a 
-      // this.cards = myarray.map(item => new Flashcard (item.name, item.front) )
-    ]
+    this.cards = []
+    this.cards = hiraganaArray.map(item => new FlashCard(item[0], item[1], item[2]))
+    console.log(this.cards)
     this.index = 0
     this.currentCard = this.cards[this.index]
-    this.cardDiv = document.querySelector('.card')
-
-    this.hintButton = document.querySelector('.hint')
-    this.hintButton.addEventListener('click', () => {
-      this.currentCard.showHint()
-    })
-
-    this.flipButton = document.querySelector('.flip')
-    this.flipButton.addEventListener('click', () => {
-      this.flip()
-    })
-
-    this.nextButton = document.querySelector('.right')
-    this.nextButton.addEventListener('click', () => {
-      this.next()
-    })
-
-    this.prevButton = document.querySelector('.left')
-    this.prevButton.addEventListener('click', () => {
-      this.goToPrevious()
-    })
-
     this.highestScore = 0
-    this.possibleScores = []
+    this.scoreRange = []
+    this.cardDiv = document.querySelector('.card')
   }
-
   flip () {
     this.currentCard.checkAnswer()
     this.currentCard.showBack()
-    // this.cardDiv.style.background = "url('images/index-card.png')"
-    // this.cardDiv.style.backgroundSize = 'cover'
     this.cardDiv.classList.add('shadow')
     this.currentCard.display.classList.add('handwritten')
     this.currentCard.display.style.fontSize = '140px'
@@ -136,26 +68,17 @@ class Game {
     this.addScoreHeaders()
     this.arrangeScores()
   }
-
   next () {
     // increase the index by one and then clear and place new card
     this.index = this.index + 1
     this.currentCard = this.cards[this.index]
     this.checkIfAtEnd()
-    this.clear()
+    this.currentCard.clear()
     // this.cardDiv.style.background = ''
     // this.cardDiv.style.backgroundColor = 'white'
     this.cardDiv.classList.remove('shadow')
     this.currentCard.showFront()
   }
-
-  clear () {
-    // clear the hint, input, and result areas
-    this.currentCard.input.value = ''
-    this.currentCard.result.style.display = 'none'
-    this.currentCard.hintText.innerHTML = ''
-  }
-
   checkIfAtEnd () {
     // shuffle and sort by score if at end. Otherwise, remove end-game text
     if (this.index === this.cards.length) {
@@ -168,7 +91,6 @@ class Game {
       this.currentCard.gameEnd.style.display = 'none'
     }
   }
-
   shuffle () {
     // shuffle the cards in a random order
     this.cards.sort(function (a, b) {
@@ -176,7 +98,6 @@ class Game {
     })
     console.log(this.cards)
   }
-
   sortByScore () {
     // sort the cards in order of lowest to highest score, putting incorrect cards at the front
     this.cards.sort(function (a, b) {
@@ -184,7 +105,6 @@ class Game {
     })
     console.log(this.cards)
   }
-
   goToPrevious () {
     // if on back of card, flip over to front. If on front of card, go to previous card front
     if (this.currentCard.display.innerText === this.currentCard.back) {
@@ -192,39 +112,34 @@ class Game {
     } else {
       this.index = this.index - 1
       this.currentCard = this.cards[this.index]
-      this.clear()
+      this.currentCard.clear()
       this.currentCard.showFront()
     }
   }
-
   checkHighScore () {
     if (this.highestScore < Math.max(...this.cards.map((i) => i.score))) {
       this.highestScore = Math.max(...this.cards.map((i) => i.score))
-      this.possibleScores = []
+      this.scoreRange = []
       for (let i = this.highestScore; i > -1; i--) {
-        this.possibleScores.push(i)
+        this.scoreRange.push(i)
       }
     } else {}
   }
-
   addScoreHeaders () {
     let scoreDiv = document.querySelector('aside div')
     scoreDiv.innerHTML = ''
-    this.possibleScores.forEach((score) => {
+    this.scoreRange.forEach((score) => {
       let scoreHeader = document.createElement('h3')
       scoreHeader.innerHTML = `${score} time(s) correct:`
       scoreHeader.setAttribute('data-score', score)
       scoreDiv.appendChild(scoreHeader)
     })
   }
-
   arrangeScores () {
-    this.possibleScores.forEach((score) => {
+    this.scoreRange.forEach((score) => {
       let cardsWithSameScore = this.cards.filter((card) => card.score === score)
       let frontsWithSameScore = []
-      cardsWithSameScore.forEach((card) => {
-        frontsWithSameScore.push(card.front)
-      })
+      cardsWithSameScore.forEach((card) => { frontsWithSameScore.push(card.front) })
       let spanScores = document.createElement('ul')
       spanScores.innerHTML = frontsWithSameScore
       document.querySelector(`h3[data-score*="${score}"]`).appendChild(spanScores)
@@ -235,23 +150,9 @@ class Game {
 class Display {
   constructor () {
     this.game = new Game()
-    this.keyPressed = null
-    this.keyLocation = null
-    window.addEventListener('keyup', (event) => {
-      this.keyPressed = event
-      this.keyLocation = event.path[0].classList[0]
-      if (this.keyPressed.key === 'ArrowLeft' && this.keyLocation !== 'input') {
-        this.arrowLeft()
-      } else if (this.keyPressed.key === 'ArrowRight' && this.keyLocation !== 'input') {
-        this.arrowRight()
-      } else if (this.keyPressed.key === 'h' && this.keyLocation !== 'input') {
-        this.h()
-      } else if (this.keyPressed.key === 'Enter') {
-        this.enter()
-      }
-    })
     this.asides = document.querySelectorAll('aside')
     this.asides.forEach((aside) => {
+      // create hover effect on left side bar
       aside.addEventListener('mouseover', () => {
         aside.classList.add('show')
         aside.lastElementChild.classList.add('show')
@@ -262,25 +163,48 @@ class Display {
         aside.lastElementChild.classList.remove('show')
       })
     })
-  }
-  arrowLeft () {
-    this.game.goToPrevious()
-  }
+    this.keyPressed = null
+    this.cursorLocation = null
+    window.addEventListener('keyup', (event) => {
+      // Add listener for ArrowLeft, ArrowRight, h, and Enter to execute various functions
+      this.keyPressed = event
+      this.cursorLocation = event.path[0].classList[0]
+      if (this.keyPressed.key === 'ArrowLeft' && this.cursorLocation !== 'input') {
+        this.game.goToPrevious()
+      } else if (this.keyPressed.key === 'ArrowRight' && this.cursorLocation !== 'input') {
+        this.game.next()
+      } else if (this.keyPressed.key === 'h' && this.cursorLocation !== 'input') {
+        this.game.currentCard.showHint()
+      } else if (this.keyPressed.key === 'Enter') {
+        this.game.flip()
+      }
+    })
+    this.hintButton = document.querySelector('.hint')
+    this.hintButton.addEventListener('click', () => this.game.currentCard.showHint())
 
-  arrowRight () {
-    this.game.next()
-  }
+    this.flipButton = document.querySelector('.flip')
+    this.flipButton.addEventListener('click', () => this.game.flip())
 
-  h () {
-    this.game.currentCard.showHint()
-  }
+    this.nextButton = document.querySelector('.right')
+    this.nextButton.addEventListener('click', () => this.game.next())
 
-  enter () {
-    this.game.flip()
+    this.prevButton = document.querySelector('.left')
+    this.prevButton.addEventListener('click', () => this.game.goToPrevious())
   }
 }
 
 const display = new Display()
 
-// add listeners to create hover with on keyboard shortcuts and 
+// add listeners to create hover with on keyboard shortcuts and
 // Add media queries to be mobile friendly
+
+// Figure out a way for the hint text to render as Bold and Red...
+// test CSS, HTML, and JS in a code review thing
+// write the README
+// If you have time, start working on a media query:
+// // Eliminate the shortcuts bar
+// // Resize the Instructions div and move it to the top of the page
+// // Resize the Score div and move it to the bottom of the page
+// // Change both animations and listeners for the above
+// // Resize the main boxes
+// // Use smaller font sizes on everything
